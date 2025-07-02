774312324a31841e6eee01449120c9bacf5c1c9b
Praneeth Lakshman <praneethejedi@gmail.com>
2025-07-02 19:01:52 +0100

    Initial commit - added two algs in cpp

---
+++ rk45.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+// dy/dt = log(abs(y)) - cos(t)
+double dydt(double y, double t) {
+    return std::log(std::abs(y)) - std::cos(t);
+}
+
+double next_step(double y, double h, double t) {
+    const int c_
+
+    return y + ((k_1 + 2 * k_2 + 2* k_3 + k_4) / 6);
+}
